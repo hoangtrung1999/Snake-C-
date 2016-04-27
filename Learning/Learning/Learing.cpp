57969d27bfa0a6b733d00eb10b0dc027c098ce23
hoangtrung1999 <hoangtrung1999@yahoo.com.vn>
2016-04-27 21:11:48 +0700

    Complete game Snake with C++

--- Learning/Learning/Learing.cpp
+++ Learning/Learning/Learing.cpp
@@ -1,4 +1,11 @@
-
+/* If (this_code_run == true) 
+it's_creator's_name = "Nguyen Hoang Trung";
+	else
+it's_creator's_name = "Undefine";
+I'm a game programmer , contact me :v 
+facebook.com/hoangtrung1999
+facebook.com/COmbOstudios
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_DEPRECATE
 
@@ -8,8 +15,9 @@
 #include <time.h>
 #include <Windows.h>
 
-
-
+#define Height 80 
+#define Width 13
+#define Length 69
 using namespace std;
 
 enum TrangThai {Up , Down , Left , Right};
@@ -22,7 +30,7 @@ struct Toado
 
 struct Snake
 {
-	Toado dot[31];//toa do dot
+	Toado dot[Length];//
 	int n;//so dot
 	TrangThai tt;
 };
@@ -50,11 +58,11 @@ void ShowSnake (Snake snake , Fruit fr)
 	clrscr();
 	//
 	TextColor(2);
-	for ( int i = 0 ; i < 13 ; i++ ) // truc Oy
+	for ( int i = 0 ; i < Width ; i++ ) //  Oy
 	{
-		if ( i == 0 || i == 12  )
+		if ( i == 0 || i == Width-1  )
 		{
-			for ( int j = 0 ; j < 80 ; j++ ) // Truc Ox
+			for ( int j = 0 ; j < Height ; j++ ) //  Ox
 			{
 					
 					cout<<(char) 4;
@@ -62,9 +70,9 @@ void ShowSnake (Snake snake , Fruit fr)
 		}
 		else 
 		{
-			for ( int k = 0 ; k < 80 ; k++)
+			for ( int k = 0 ; k < Height ; k++)
 			{
-				if ( k == 0 || k == 79)
+				if ( k == 0 || k == Height -1)
 				{
 					cout<<(char) 4;
 				}
@@ -96,7 +104,7 @@ void ShowSnake (Snake snake , Fruit fr)
 	gotoXY(fr.td.x , fr.td.y);
 	cout<<"U";
 }
-
+//
 void MoveSnake (Snake &snake)
 {
 	for (int i = snake.n -1 ; i > 0 ; i--)
@@ -144,12 +152,13 @@ void MoveSnake (Snake &snake)
 	
 
 }
-
-void Solve (Snake &snake , Fruit &fr)
+//
+void Solve (Snake &snake , Fruit &fr , int *p)
 {
 	if ( snake.dot[0].x == fr.td.x &&
 		snake.dot[0].y == fr.td.y)
 	{
+		*p = *p +1 ;
 		snake.n++;
 		for (int i = 1; i < snake.n ; i++)
 		{
@@ -172,46 +181,19 @@ void Solve (Snake &snake , Fruit &fr)
 		{
 			snake.dot[0].x--;
 		}
-		fr.td.x = rand() % 80;
-		fr.td.y = rand() % 25;
+		fr.td.x = (rand() % Height);
+		fr.td.y = (rand() % Width);
 	}
 
 	
-	
-	/*if ( snake.tt == Down && snake.dot[0].y == 25 )
-	{
-		for ( int i = 0 ; i <= snake.n ; i++ )
-		{
-			snake.dot[i].y = snake.dot[i].y -25;
-		}
-	}
-	if ( snake.tt == Up && snake.dot[0].y == 0 )
-	{
-		for ( int i = 0 ; i <= snake.n ; i++ )
-		{
-			snake.dot[i].y = snake.dot[i].y +25;
-		}
-	} 
-	if ( snake.tt == Right && snake.dot[0].x == 80 )
-	{
-		for ( int i = 0 ; i <= snake.n ; i++ )
-		{
-			snake.dot[i].x = snake.dot[i].x -80;
-		}
-	}
-	if ( snake.tt == Left && snake.dot[0].x == 0 )
-	{
-		for ( int i = 0 ; i <= snake.n ; i++ )
-		{
-			snake.dot[i].x = snake.dot[i].x + 80;
-		}
-	} */
+
 }
-//IN line
-
+//
 
 int main()
 {
+	int Score = 0;
+	int *p = &Score;
 	BOOL isTouch = false;
 
 	srand(time(NULL));
@@ -233,19 +215,26 @@ int main()
 			}
 
 	}
-	//Show 
+	if ( snake.dot[0].x == 0 || snake.dot[0].x == Height
+		|| snake.dot[0].y == 0 || snake.dot[0].y == Width)
+	{
+		isTouch = true;
+	}
+	//
 	ShowSnake(snake, fr);
 	
-	// Di chuyen snake
+	//
 	MoveSnake(snake);
 
-	//Xu ly
-	Solve(snake , fr);
+	//
+	Solve(snake , fr , &Score );
 	Sleep(100);
 	}
 
 	clrscr();
 	cout<<"You Lose\n";
+	cout<<"Your Score: "<<Score<<endl;
 
 	return 0;
+	_getch();
 }
